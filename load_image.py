88effcfe9b639ee1df551a0e35378f605fc6c3ea
yuiwu0828 <86836761+yuiwu0828@users.noreply.github.com>
2022-05-01 17:46:27 +0800

    讀入圖片
    每種最多讀400張，存成圖片和對應編號的tuple(圖片經過固定成36*16的大小和轉灰階)

---
+++ load_image.py
@@ -0,0 +1,29 @@
+import cv2, os, sys
+import numpy as np
+
+os.chdir(sys.path[0])
+
+def load_image(dataPath):
+    dataset = []
+    folder = os.listdir(dataPath)
+    ID = 0
+    for folder_name in folder:
+        files = os.listdir(dataPath + '/' + folder_name)
+        count = 0
+        for name in files:
+            img = cv2.imread(dataPath + '/' + folder_name + '/' + name)
+            img = cv2.resize(img, (36, 16))
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            element = (np.array(img), 1)
+            dataset.append((element, ID))
+            count = count + 1
+            if count >= 400:
+                break
+        ID = ID + 1
+
+    return dataset
+
+if __name__ == '__main__':
+    dataPath = 'C:/Users/User/Desktop/final_project/used/Train'
+    load_image(dataPath)
+    print('done')
